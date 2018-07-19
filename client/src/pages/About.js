957a5a182ce0be00dcf9065cab13a0d51c83cdca
cassandra <cassandradaw@gmail.com>
2018-07-19 11:05:08 +1000

    add pages HomePage, GalleryShop, SellArt, AboutUs, SellArt. Router in App.js calls them all execpt AboutUs

---
+++ client/src/pages/About.js
@@ -0,0 +1,18 @@
+// import React from 'react'
+
+// export default () => (<div>
+
+//       <h2>Our Story</h2>
+//       <ul>
+//         <li>
+//           <Link to={`${match.url}/ourstory`}>Our Story</Link>
+//         </li>
+//         <li>
+//           <Link to={`${match.url}About/ourhistory`}>Our History</Link>
+//         </li>
+//         <li>
+//           <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+//         </li>
+//   </ul>
+//   </div>
+// );
---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => (<div>
+    <h2>Virtual stuff</h2>
+    <p>Stuff ksdfhg kvhgirt gibuir</p>
+</div>
+
+);
+  
