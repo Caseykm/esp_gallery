3d43a6e138c9338a9422e2f030b16d4d4204b32b
cassandra <cassandradaw@gmail.com>
2018-07-19 14:59:00 +1000

    working on slider

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React from 'react';
+// import { UncontrolledCarousel } from 'reactstrap';
 
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+// import { Carousel } from 'react-responsive-carousel';
+// import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
+ 
+// class DemoCarousel extends Component {
+//     render() {
+//         return (
+//             <Carousel>
+//                 <div>
+//                     <img src="assets/1.jpeg" />
+//                     <p className="legend">Legend 1</p>
+//                 </div>
+//                 <div>
+//                     <img src="assets/2.jpeg" />
+//                     <p className="legend">Legend 2</p>
+//                 </div>
+//                 <div>
+//                     <img src="assets/3.jpeg" />
+//                     <p className="legend">Legend 3</p>
+//                 </div>
+//             </Carousel>
+//         );
+//     }
+// });
+
+// ReactDOM.render(<DemoCarousel />, document.querySelector('.demo-carousel'));
 export default () => (<div>
-    <h2 className="PageHeader" >Virtual Gallery</h2>
+    
+
+ 
+     <h2 className="PageHeader" >Virtual Gallery</h2>
     <h3 className="PageHeader" >Current Exhibition</h3>
-    <p>Stuff ksdfhg kvhgirt gibuir</p>
-</div>
+     <p>Stuff ksdfhg kvhgirt gibuir</p>
+ </div>
 
 );
   
