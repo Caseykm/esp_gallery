3822dc45fd20c1f0228347d7d4685f320ac9728b
cassandra <cassandradaw@gmail.com>
2018-07-19 16:42:31 +1000

    Have responcive slide...NO styling yet

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,38 +1,18 @@
 import React from 'react';
-// import { UncontrolledCarousel } from 'reactstrap';
-
-// import React, { Component } from 'react';
-// import ReactDOM from 'react-dom';
-// import { Carousel } from 'react-responsive-carousel';
-// import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
+import ReactDOM from 'react-dom';
+import { Carousel } from 'react-responsive-carousel';
+import GalleryCarousel from './GalleryCarousel';
  
-// class DemoCarousel extends Component {
-//     render() {
-//         return (
-//             <Carousel>
-//                 <div>
-//                     <img src="assets/1.jpeg" />
-//                     <p className="legend">Legend 1</p>
-//                 </div>
-//                 <div>
-//                     <img src="assets/2.jpeg" />
-//                     <p className="legend">Legend 2</p>
-//                 </div>
-//                 <div>
-//                     <img src="assets/3.jpeg" />
-//                     <p className="legend">Legend 3</p>
-//                 </div>
-//             </Carousel>
-//         );
-//     }
-// });
 
-// ReactDOM.render(<DemoCarousel />, document.querySelector('.demo-carousel'));
+ 
+// ReactDOM.render(<GalleryCarousel />, document.querySelector('.gallery-carousel'));
+
+
 export default () => (<div>
     
-
+    <GalleryCarousel />
  
-     <h2 className="PageHeader" >Virtual Gallery</h2>
+    <h2 className="PageHeader" >Virtual Gallery</h2>
     <h3 className="PageHeader" >Current Exhibition</h3>
      <p>Stuff ksdfhg kvhgirt gibuir</p>
  </div>
