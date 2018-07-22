0a6ae5eda746bf2c419061dee9c0bb22b4266c90
Chris Barfod <cbarfod@live.com.au>
2018-07-22 19:18:44 +1000

    Redux login action and set current user

---
+++ client/src/validation/is-empty.js
@@ -0,0 +1,8 @@
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  // vvv If the keys length is 0 that means it's an empty object.
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+
+export default isEmpty;
