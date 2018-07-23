c7a41c231e2e02c0a845ea109587d484dbb6bb3d
caseykm <caseykenjiro@yahoo.com>
2018-07-23 14:26:54 +1000

    finish ImageUpload

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,36 @@ import About from "./pages/About"; // Imports About.js into App.js
 import ContactUs from "./forms/ContactUs"; // Imports ContactUs.js into App.js
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import axios from 'axios'
 
+const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/dxmelc0e6/upload';
+const CLOUDINARY_UPLOAD_PRESET = 'espgallery';
 
+// cloudinary>
+window.onload=function(){
+  let imgPreview = document.getElementById('img-preview');
+  let fileUpload = document.getElementById('file-upload');
 
+fileUpload.addEventListener('change', function(event) {
+  let file = event.target.files[0];
+let formData = new FormData();
+formData.append('file', file);
+formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+
+  axios({
+    url: CLOUDINARY_URL,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data: formData
+  }).then(function(res) {
+    console.log(res);
+  }).catch(function(err) {
+    console.error(err);
+  })
+})};
+// cloudinary
 class App extends Component {
   state = {
     response: ""
@@ -71,3 +98,4 @@ class App extends Component {
 
 
 export default App;
+
